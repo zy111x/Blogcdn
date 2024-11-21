2858834d793dc9855c176bd13cb0cca77fc2239b
东方月初 <165906706+damizai@users.noreply.github.com>
2024-11-21 21:20:27 +0800

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -9,7 +9,7 @@ const urls = [
     "https://fastly.hexo.200038.xyz#Fastly CDN",
     "https://gcore.hexo.200038.xyz#Gcore CDN",
     "https://vercel.hexo.200038.xyz#Vercel CDN",
-    "https://www.weilai.us.kg#备用地址"
+    "https://weilai.us.kg#备用地址"
 ];
 
 // 动态生成URL列表
